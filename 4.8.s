5ef4dd2a3f5eb55cdaa0f2765d216ac6aff7d543
Victor <victor.vivancos@hotmail.com>
2020-03-16 19:39:31 +0100

    Completed 4.8

--- 4.8.s
+++ 4.8.s
@@ -26,4 +26,48 @@ loop:
 	bge $t0, $t3, end
 	mult $t0, $t3
 	mflo $t4
-	addiu 
+	addiu $t4, $t4, 4
+	sll $t4, $t4, 2
+	addu $t4, $t4, $a0
+	lw $t4, 0($t4)
+	addu $t2, $t2, $t4
+	addiu $t0, $t0, 1
+	b loop 
+end:
+
+#SECTION C
+#First solution 
+	la $t3, N
+	lw $t3, 0($t3)
+loop:	
+	bge $t0, $t3, end
+	addiu $t4, $t3, 1
+	mul $t4, $t0
+	mflo $t4, 
+	sll $t4, $t4, 2
+	addu $t4, $t4, $a0
+	lw $t4,0($t4)
+	addu $t2, $t2, $t4
+	addiu $t0, $t0, 1
+	b loop
+end:
+
+#SECTION D
+#First Solution 
+	la $t3, N
+	lw $t3, 0($t3)
+loop:
+	bge $t0, $t3, end
+	mult $t0, $t3
+	mflo $t4
+	addu $t4, $t4, $t3
+	addiu $t4, $t4, -1
+	subu $t4, $t4, $t0
+	sll $t4,$t4, 2
+	addu $t4, $t4, $a0
+	lw $t4,0($t4)
+	addu $t2, $t2, $t4
+	addiu $t0, $t0, 4
+	b loop
+end:
+ 
