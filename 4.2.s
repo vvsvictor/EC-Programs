738e6e88eef11f69d035826ec6ca53fcfb72eaf4
Victor <victor.vivancos@hotmail.com>
2020-03-19 21:35:08 +0100

    4.2 sll 2

--- 4.2.s
+++ 4.2.s
@@ -4,12 +4,14 @@ func:
     lw $t0, 0($t0)
     mult $a1, $t0 #i*N
     mflo $t1
-    addu $t1, $t1, $a0 #&mat + i*N
-    addu $t1, $t1, $a2 # &mat[i][j]
+    addu $t1, $t1, $a2 # i*N + j
+    sll $t1, $t1, 2
+    addu $t1, $t1, $a0 # &mat[i][j]
     mult $a2, $t0 #j*N
     mflo $t2
-    addu $t2, $t2, $a0 # &mat + i*N
-    addu $t2, $t2, $a1 #&mat[j][i]
+    addu $t2, $t2, $a1 # &mat + i*N
+    sll $t2, $t2, 2
+    addu $t2, $t2, $a0 #&mat[j][i]
     lw $t2, 0($t2) # mat[j][i]
     sw $t2, 0($t1) # mat[i][j] = mat [j][i]
 end:
---
+++ 4.9.s
@@ -0,0 +1,7 @@
+func:
+  #Section a
+  li $t0, 1 #i
+  la $t1, N
+  lw $t1, 0($t1) #N
+  la $t2, vecchar #$t2 = &vecchar
+  
